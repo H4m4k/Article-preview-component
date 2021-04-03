1d4d09eb760b7aa76ae8b1be71c336e769b56076
H4m4k <pawel.janik.1983@gmail.com>
2021-04-03 12:06:26 +0200

    mobile - minor layout changes, added wrapper container to text/share part to position it better

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const author_Button = document.querySelector('#share_Btn');
+const share_Buttton = document.querySelector('#share_Btn');
 const author_Panel = document.querySelector('#author')
 const share_Panel = document.querySelector('#share')
+const share_Wrap = document.querySelector('#share_Wrap')
 
-author_Button.addEventListener('click', show_Share);
+share_Buttton.addEventListener('click', show_Share);
 
 function show_Share() {
     author_Panel.classList.add('off')
     share_Panel.classList.remove('off')
-    author_Button.removeEventListener('click',show_Share)
+    share_Wrap.classList.add('dark_Bg')
+    share_Buttton.removeEventListener('click', show_Share);
+    share_Buttton.addEventListener('click', show_Author);
 }
 
 function show_Author() {
     share_Panel.classList.add('off')
     author_Panel.classList.remove('off')
+    share_Wrap.classList.remove('dark_Bg')
+    share_Buttton.addEventListener('click', show_Share);
 }
